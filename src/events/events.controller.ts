172c6dea6b00a63e8d609253e5426ba33fb7b2bc
Saulo Machado <saulo.machado@g.globo>
2022-05-01 19:24:54 -0300

    Aulas 1..10: Configurando projeto e criando controller events

---
+++ src/events/events.controller.ts
@@ -0,0 +1,25 @@
+import { Controller, Get, Post, Patch, Delete, Param } from '@nestjs/common';
+
+@Controller('events')
+export class EventsController {
+  @Get()
+  findAll() {
+      return 'FindAll called successfully';
+  }
+  @Get(':id')
+  findOne(@Param('id') id) {
+    return id;
+  }
+  @Post()
+  create() {
+    return 'Create called successfully';
+  }
+  @Patch()
+  update() {
+    return 'Update called successfully';
+  }
+  @Delete()
+  remove() {
+    return 'Remove called successfully';
+  }
+}
